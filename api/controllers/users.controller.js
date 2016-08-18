ac4dff3b685f22b785c0b413471e3f1b8944e987
Andy Eccleston <eandy5000@yahoo.com>
2016-08-18 18:51:24 -0500

    users/login working

--- api/controllers/users.controller.js
+++ api/controllers/users.controller.js
@@ -1,56 +1,53 @@
 var mongoose = require('mongoose');
-var User = mongoose.model('User');
-var bcrypt = require('bcrypt-nodejs');
+var User     = mongoose.model('User');
+var bcrypt   = require('bcrypt-nodejs');
+//var jwt      = require('jsonwebtoken');
 
-module.exports.register = function(req,res) {
-    console.log('registering users');
-    var username = req.body.username;
-    var name = req.body.name || null;
-    var password = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+module.exports.register = function(req, res) {
+  console.log('registering user');
 
-    User.create({
-        username : username,
-        name : name,
-        password : password
-    }, function(err, user){
-        if(err) {
-            console.log(err);
-            res
-                .status(400)
-                .json(err);
-        } else {
-            console.log("user created ", user);
-            res
-                .status(201)
-                .json(user);
-        }
+  var username = req.body.username;
+  var name = req.body.name || null;
+  var password = req.body.password;
 
-    });
+  User.create({
+    username: username,
+    name: name,
+    password: bcrypt.hashSync(password, bcrypt.genSaltSync(10))
+  }, function(err, user) {
+    if (err) {
+      console.log(err);
+      res.status(400).json(err);
+    } else {
+      console.log('user created', user);
+      res.status(201).json(user);
+    }
+  });
 };
 
 module.exports.login = function(req, res) {
-    console.log("logging in user");
-    var username = req.body.username;
-    var password = req.body.password;
+  console.log('logging in user');
+  var username = req.body.username;
+  var password = req.body.password;
 
-    User.findOne({
-        username : username
-    })
-        .exec(function(err, user){
-            if(err){
-                console.log(err);
-                res.status(400).json(err);
-            } else {
-                console.log('compareSync ',bcrypt.compareSync(password, user.password));
-                if(bcrypt.compareSync(password, user.password)){
-                    console.log('found user ', user);
-                    res.status(200).json(user);
-                } else {
-                    console.log("unauthorized password", password," user.password",user.password);
-                    res.status(401).json({"message": "unauthorized"});
-                }
-                
-            }
-        });
+  User.findOne({
+    username: username
+  }).exec(function(err, user) {
+    if (err) {
+      console.log(err);
+      res.status(400).json(err);
+    } else {
+      if (bcrypt.compareSync(password, user.password)) {
+        console.log('User found', user);
+ //       var token = jwt.sign({ username: user.username }, 's3cr3t', { expiresIn: 3600 });
+        res.status(200).json(user);
+      } else {
+        res.status(401).json('Unauthorized');
+      }
+    }
+  });
+};
+
+module.exports.authenticate = function(req, res, next) {
 
 };
